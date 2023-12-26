1c4de8cfe86b746ea0969c8c24faef64ff33fc46
Jakob Meyer <jakobmeyer.feu@gmail.com>
2023-12-26 16:04:53 +0100

    rTree bugfixing, searching for edges not vertices

--- src/GraphXings/NewFiles/BetterEdgeCrossingRTree.java
+++ src/GraphXings/NewFiles/BetterEdgeCrossingRTree.java
@@ -1,11 +1,9 @@
 package GraphXings.NewFiles;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.TreeSet;
 
 import com.github.davidmoten.rtree2.Entry;
 import com.github.davidmoten.rtree2.RTree;
@@ -120,12 +118,13 @@ public class BetterEdgeCrossingRTree {
     public int calculateCrossings(Vertex vertex, HashMap<Vertex, Coordinate> mapVertexToCoordinate) {
         int crossings = 0;
 
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 0;
+        int x1 = Integer.MAX_VALUE;
+        int y1 = Integer.MAX_VALUE;
+        int x2 = Integer.MIN_VALUE;
+        int y2 = Integer.MIN_VALUE;
 
-        // iterate over the edges adjacent to this vertex
+        // iterate over the edges adjacent to this vertex, e.g. the ones that get drawn
+        ArrayList<Edge> createdEdges = new ArrayList<>();
         Iterable<Edge> incidentEdges = this.g.getIncidentEdges(vertex);
         for (Edge edge : incidentEdges) {
             Coordinate s = mapVertexToCoordinate.get(edge.getS());
@@ -136,6 +135,9 @@ public class BetterEdgeCrossingRTree {
                 continue;
             }
 
+            // add the edge to check possible resulting crossings
+            createdEdges.add(edge);
+
             // update the above-scope variables to the new mins/maxs
             x1 = Integer.min(x1, Integer.min(s.getX(), t.getX()));
             y1 = Integer.min(y1, Integer.min(s.getY(), t.getY()));
@@ -145,102 +147,46 @@ public class BetterEdgeCrossingRTree {
             this.rTree = this.rTree.add(edge, Geometries.line(s.getX(), s.getY(), t.getX(), t.getY()));
         }
 
-        HashSet<Vertex> searchedVertices = new HashSet<>();
-
-        Rectangle rectangle = Geometries.rectangle(x1, y1, x2, y2);
-        Iterable<Entry<Edge, Geometry>> search = this.rTree.search(rectangle);
-        for (Entry<Edge, Geometry> entry : search) {
-            searchedVertices.add(entry.value().getS());
-            searchedVertices.add(entry.value().getT());
+        // in the case that we do not have any incident edges that are created
+        // (therefore the x1, ... are not changed), we return 0 crossings!
+        // note: we could check for the size of incidentEdges, but as this is an
+        // Iterable and we would have to work with another variable or StreamSupport,
+        // this solution seems faster
+        if (x1 > x2) {
+            return crossings;
         }
 
-        // comparator: compares edges for the higher y-value in any of their two nodes
-        // true if e1 has higher y-value, else false
-        Comparator<Edge> comparator = (Edge e1, Edge e2) -> {
-            return Integer.compare(
-                    Math.max(mapVertexToCoordinate.get(e1.getS()).getY(), mapVertexToCoordinate.get(e1.getT()).getY()),
-                    Math.max(mapVertexToCoordinate.get(e2.getS()).getY(), mapVertexToCoordinate.get(e2.getT()).getY()));
-        };
-        // create a TreeSet where you automatically sort the added edges by the y-value
-        TreeSet<Edge> binarySearchTree = new TreeSet<Edge>(comparator);
+        // search all edges in the rTree in the rectangle that encloses all the newly
+        // created edges from the placed vertex
+        // (!) we know the created ones don't self intersect, so we keep them separate
+        Rectangle rectangle = Geometries.rectangle(x1, y1, x2, y2);
+        Iterable<Entry<Edge, Geometry>> search = this.rTree.search(rectangle);
+        HashSet<Edge> foundEdges = new HashSet<>();
+        for (Entry<Edge, Geometry> entry : search) {
+            if (!createdEdges.contains(entry.value())) {
+                foundEdges.add(entry.value());
+            }
+        }
 
-        for (Vertex v : searchedVertices) {
-            Iterable<Edge> adjacentEdges = this.g.getIncidentEdges(v);
-            for (Edge edge : adjacentEdges) {
-                // for the current edge get the neighbor
-                Vertex neighbor = v == edge.getS() ? edge.getT() : edge.getS();
-                // TODO only the placed vertex from which we get the searchedVertices is valid
-                // skip this neighbor if it wasn't placed yet
-                if (mapVertexToCoordinate.get(neighbor) == null) {
-                    continue;
-                }
+        // every (distinct) found edge is checked for crossings
+        for (Edge foundEdge : foundEdges) {
+            Coordinate s_coord = mapVertexToCoordinate.get(foundEdge.getS());
+            Coordinate t_coord = mapVertexToCoordinate.get(foundEdge.getT());
+            Segment foundSegment = new Segment(s_coord, t_coord);
 
-                // create a segment from the current edge for the intersection check
-                Segment thisSegment = new Segment(mapVertexToCoordinate.get(edge.getS()),
-                        mapVertexToCoordinate.get(edge.getT()));
-
-                // if the vertex is positioned left (x-axis) of its neighbor
-                // compute whether there is a crossing
-                if (mapVertexToCoordinate.get(v).getX() < mapVertexToCoordinate.get(neighbor).getX()) {
-                    // (!) add the edge to the search tree if we sweep over the line from now on
-                    binarySearchTree.add(edge);
-
-                    // get the next higher edge (y-axis) after the current edge
-                    Edge higherEdge = binarySearchTree.higher(edge);
-                    // if that edge is valid (there is a higher edge one than the current edge)
-                    if (higherEdge != null) {
-                        // create another segment from this predicted edge
-                        Segment higherSegment = new Segment(mapVertexToCoordinate.get(higherEdge.getS()),
-                                mapVertexToCoordinate.get(higherEdge.getT()));
-                        // if the segments intersect, we increment the crossing number and check for the
-                        // next neighbor
-                        if (Segment.intersect(thisSegment, higherSegment)) {
-                            crossings++;
-                            continue;
-                        }
+            // with each of the newly created edges
+            for (Edge createdEdge : createdEdges) {
+                if (!foundEdge.equals(createdEdge)) {
+                    Segment createdSegment = new Segment(mapVertexToCoordinate.get(createdEdge.getS()),
+                            mapVertexToCoordinate.get(createdEdge.getT()));
+                    if (Segment.intersect(foundSegment, createdSegment)) {
+                        crossings++;
                     }
-                    // analogous as for .higher() above, get the next lower edge (y-axis) before the
-                    // current edge and check if there is a crossing
-                    Edge lowerEdge = binarySearchTree.higher(edge);
-                    if (lowerEdge != null) {
-                        Segment lowerSegment = new Segment(mapVertexToCoordinate.get(lowerEdge.getS()),
-                                mapVertexToCoordinate.get(lowerEdge.getT()));
-                        if (Segment.intersect(thisSegment, lowerSegment)) {
-                            crossings++;
-                            continue;
-                        }
-                    }
-                }
-                // analogous to the other case, if the vertex is positioned right (x-axis) of
-                // its neighbor
-                else {
-                    Edge higherEdge = binarySearchTree.higher(edge);
-                    if (higherEdge != null) {
-                        Segment higherSegment = new Segment(mapVertexToCoordinate.get(higherEdge.getS()),
-                                mapVertexToCoordinate.get(higherEdge.getT()));
-                        if (Segment.intersect(thisSegment, higherSegment)) {
-                            crossings++;
-                            continue;
-                        }
-                    }
-                    Edge lowerEdge = binarySearchTree.lower(edge);
-                    if (lowerEdge != null) {
-                        Segment lowerSegment = new Segment(mapVertexToCoordinate.get(lowerEdge.getS()),
-                                mapVertexToCoordinate.get(lowerEdge.getT()));
-                        if (Segment.intersect(thisSegment, lowerSegment)) {
-                            crossings++;
-                            continue;
-                        }
-                    }
-
-                    // (!) delete the neighbor from the search tree if we are finished sweeping over
-                    // the edge
-                    binarySearchTree.remove(edge);
                 }
             }
         }
+
         // return the number of crossings with the given vertex in the current gamestate
         return crossings;
     }
-
 }
